876fcb39ddd57f3ad77be5d2d7b18d6524a9b4aa
EllisYarboi <ellisyarboi@gwmail.gwu.edu>
2015-11-23 14:14:26 -0500

    Commit to github
    in this commit you will find my source code

---
+++ Project Code/src/findingNote/ComplexNum.java
@@ -0,0 +1,62 @@
+package findingNote;
+
+public class ComplexNum {
+
+	private double realNum;
+	private double imagNum;
+
+	public ComplexNum()
+	{
+		realNum = 0.0;
+		imagNum = 0.0;
+	}
+
+	public ComplexNum(double real, double imaginary)
+	{
+		realNum = real;
+		imagNum = imaginary;
+	}
+
+	
+
+	public double getReal()		
+		{return realNum;}
+
+	public double getImaginary()		
+		{return imagNum;}
+
+
+	public void setReal(double real)		
+		{realNum = real;}
+	
+	public void setImaginary(double imaginary)		
+		{imagNum = imaginary;}
+	
+
+	
+
+	public ComplexNum add(ComplexNum c2)
+	{
+		ComplexNum newComplexNum = new ComplexNum();
+		newComplexNum.realNum = this.realNum + c2.realNum;
+		newComplexNum.imagNum = this.imagNum + c2.imagNum;
+
+		return newComplexNum;
+	}
+
+
+	public ComplexNum multiply(ComplexNum c2)
+	{
+		ComplexNum newComplexNum = new ComplexNum();
+		newComplexNum.realNum = (this.realNum * c2.realNum) - (this.imagNum * c2.imagNum);
+		newComplexNum.imagNum = (this.realNum * c2.imagNum) + (this.imagNum * c2.realNum);
+
+		return newComplexNum;
+	}
+
+	public double magnitude()
+	{
+		return (Math.sqrt(Math.pow(this.realNum, 2) + Math.pow(this.imagNum, 2)));
+	}
+
+}
